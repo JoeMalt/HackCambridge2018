d17c4470923174a326f21c660265752c155522fc
PeterRugg <pdr32@cam.ac.uk>
2018-01-20 14:11:24 +0000

    Initial puzzle generation

---
+++ puzzle-gen/puzzle-gen.js
@@ -0,0 +1,38 @@
+var OPERATIONS = [
+	{label: " + ",
+	 min: 1000,
+	 max: 10000,
+	 order: false
+	},
+	{
+	 label: " - ",
+	 min: 1000,
+	 max: 10000,
+	 order: true
+	},
+	{
+	 label: " * ",
+	 min: 100,
+	 max: 1000,
+	 order: false
+	}
+];
+
+function genPuzzle() {
+	var opIndex = -1
+	do
+	{
+		opIndex = Math.floor(Math.random() * OPERATIONS.length)
+	} while (opIndex < 0 || opIndex >= OPERATIONS.length)
+		
+	var firstOperand = Math.floor(Math.random() * (OPERATIONS[opIndex].max - OPERATIONS[opIndex].min) + OPERATIONS[opIndex].min);
+	var secondOperand = Math.floor(Math.random() * (OPERATIONS[opIndex].max - OPERATIONS[opIndex].min) + OPERATIONS[opIndex].min);
+	if (OPERATIONS[opIndex].order && firstOperand < secondOperand)
+	{
+		var temp = firstOperand;
+		firstOperand = secondOperand;
+		secondOperand = temp;
+	}
+	
+	return( firstOperand.toString() + OPERATIONS[opIndex].label + secondOperand.toString());	
+}
