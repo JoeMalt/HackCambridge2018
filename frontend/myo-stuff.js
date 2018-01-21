c68af12e5533fccecbcd6b5d4259e1c2ee303724
zlotnleo <zlotnleo@gmail.com>
2018-01-21 01:25:17 +0000

    Added MYO functionality

---
+++ frontend/myo-stuff.js
@@ -0,0 +1,51 @@
+Myo.connect("uk.ac.cam.keepsmiling");
+
+Myo.on("connected", (data, timestamp) => {
+	console.log("Myo successfully connected. Data: " + JSON.stringify(data) + ". Timestamp: " + timestamp + ".");
+	Myo.setLockingPolicy("none")
+});
+
+var num = 0;
+
+function lerp(v0, v1, t) {
+	return v0*(1-t)+v1*t
+}
+
+function limit(min, max, val) {
+	return val < min ? min : val > max ? max : val;
+}
+
+const SHIFT_REG_LEN = 50;
+var shiftReg = [];
+
+Myo.on("orientation", (data) => {
+	let phi = Math.atan2(2 * (data.w * data.x + data.y * data.z), 1 - 2 * (data.x * data.x + data.y * data.y));
+	let digit = limit(0, 9, Math.floor(10 * lerp(0.7, -0.7, phi)));
+
+	shiftReg.push(digit);
+	shiftReg.splice(0, shiftReg.length - SHIFT_REG_LEN);
+
+	num = Math.floor(num / 10) * 10 + digit;
+	// document.getElementById("someText").innerHTML = num.toString();
+});
+
+Myo.on("fist", () => {
+	let digits = [];
+	for(var i = 0; i < 10; i++){
+		digits[i] = 0;
+	}
+	shiftReg.forEach(d => digits[d]++);
+	imax = 0;
+	for(var i = 1; i < 10; i++)
+		if(digits[i] > digits[imax])
+			imax = i;
+	num = (Math.floor(num / 10) * 10 + imax) * 10;
+	shiftReg = []
+})
+
+Myo.on("fingers_spread", () => {
+	console.log("Orientation reset!")
+	Myo.myos.map(myo => myo.zeroOrientation());
+})
+
+// myMyo.on("fist", () => {alert("Fist!");})
--- frontend/progress-bar.js
+++ frontend/progress-bar.js
@@ -3,45 +3,52 @@ const YELLOW_THRESHOLD = 50;
 
 var total_seconds;
 
-var out_of_time_callback;
+var finishedBarCallback;
 
-var countdown_interval;
+var updateInterval;
 
 var barInitTime;
-var barFinishTime;
 
 function initialiseProgressBar(seconds, out_of_time_callback){
-    console.log("initialising progress bar");
     total_seconds = seconds;
     barInitTime = Date.now();
-    barFinishTime = barInitTime + 1000 * total_seconds;
-    out_of_time_callback = out_of_time_callback;
+    finishedBarCallback = out_of_time_callback;
     updateProgressBar();
-    setInterval(updateProgressBar, 100);
+    updateInterval = setInterval(updateProgressBar, 100);
 }
 
 function updateProgressBar(){
-    var percentage = (100 - 100 * ((Date.now() - barInitTime) / (1000 * total_seconds))).toFixed(2);
 
-    console.log("Updating progress bar to " + percentage + "%");
+    var percentage = (100 - 100 * ((Date.now() - barInitTime) / (1000 * total_seconds))).toFixed(2);
+    var stopFlag = false;;
+
+    if(stopFlag = (percentage <= 0.0)){
+        clearInterval(updateInterval);
+        percentage = 0
+    }
+    // console.log("Updating progress bar to " + percentage + "%");
     $('#timer-progress-bar').css({ width : percentage + "%"}); 
     
     if (percentage > YELLOW_THRESHOLD){
-        console.log("Setting progress bar to green");
+        // console.log("Setting progress bar to green");
         $('#timer-progress-bar').addClass('bg-success');
         $('#timer-progress-bar').removeClass('bg-warning');
         $('#timer-progress-bar').removeClass('bg-error');
     }
     else if (percentage > RED_THRESHOLD){
-        console.log("Setting progress bar to yellow");
+        // console.log("Setting progress bar to yellow");
         $('#timer-progress-bar').removeClass('bg-success');
         $('#timer-progress-bar').addClass('bg-warning');
         $('#timer-progress-bar').removeClass('bg-error');
     }
     else{
-        console.log("Setting progress bar to red");
+        // console.log("Setting progress bar to red");
         $('#timer-progress-bar').removeClass('bg-success');
         $('#timer-progress-bar').removeClass('bg-warning');
         $('#timer-progress-bar').addClass('bg-danger');
+    }
+
+    if(stopFlag){
+        finishedBarCallback();
     }
 }
