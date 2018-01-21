37ec1134c284997e3c9b8dfaf5793bc303026503
zlotnleo <zlotnleo@gmail.com>
2018-01-21 03:41:10 +0000

    Added digit dial

---
+++ frontend/digit.js
@@ -0,0 +1,35 @@
+function initDial(){
+	var offset = 0.26;
+	var canvas = document.getElementById("digitDial");
+	var w = canvas.width;
+	var h = canvas.height;
+	var context = canvas.getContext("2d");
+	context.font = "24px Arial";
+	redraw = () => {
+		context.clearRect(0, 0, w, h);
+		context.lineWidth = 1;
+		var x, y, angle;
+		for(var i = 0; i < 10; i++) {
+			angle = offset + (Math.PI - 2 * offset) * i / 9;
+			var textMetrics = context.measureText(i.toString());
+			x = w / 2 + 0.85 * w / 2 * Math.cos(angle) - textMetrics.width / 2;
+			y = h - 0.85 * h * Math.sin(angle) + parseInt(context.font) / 2;
+			if(i == Math.floor(digitFloatValue)) {
+				context.strokeStyle = "#0000CC";
+			} else {
+				context.strokeStyle = "#000000";
+			}
+			context.strokeText(i.toString(), x, y);
+		}
+		context.strokeStyle = "#FF0000";
+		context.lineWidth = 10;
+		context.beginPath();
+		context.moveTo(w / 2, h);
+		angle = offset + (Math.PI - 2 * offset) * digitFloatValue / 10
+		x = w / 2 + 0.6 * w / 2 * Math.cos(angle);
+		y = h - 0.6 * h * Math.sin(angle);
+		context.lineTo(x, y);
+		context.stroke();
+	}
+	setInterval(redraw, 100);
+}
--- frontend/myo-stuff.js
+++ frontend/myo-stuff.js
@@ -1,3 +1,5 @@
+var epsilon = 0.000001;
+
 var myoConnected = false;
 
 Myo.connect("uk.ac.cam.keepsmiling");
@@ -8,10 +10,15 @@ Myo.on("connected", (data, timestamp) => {
 	Myo.setLockingPolicy("none")
 });
 
-var num = 0;
+// var num = 0;
+var digitFloatValue = 0;
 
 function lerp(v0, v1, t) {
 	return v0*(1-t)+v1*t
+}
+
+function invLerp(v0, v1, v) {
+	return (v - v0) / (v1 - v0)
 }
 
 function limit(min, max, val) {
@@ -23,13 +30,16 @@ var shiftReg = [];
 
 Myo.on("orientation", (data) => {
 	let phi = Math.atan2(2 * (data.w * data.x + data.y * data.z), 1 - 2 * (data.x * data.x + data.y * data.y));
-	let digit = limit(0, 9, Math.floor(10 * lerp(0.7, -0.7, phi)));
+	// console.log(phi);
+	// phi = 0;
+	digitFloatValue = limit(0, 10 - epsilon, 10 * invLerp(0.75, -0.75, phi));
+	// let digit = limit(0, 9, Math.floor(10 * lerp(0.7, -0.7, phi)));
+	let digit = Math.floor(digitFloatValue);
 
 	shiftReg.push(digit);
 	shiftReg.splice(0, shiftReg.length - SHIFT_REG_LEN);
 
-	num = Math.floor(num / 10) * 10 + digit;
-	// document.getElementById("someText").innerHTML = num.toString();
+	// num = Math.floor(num / 10) * 10 + digit;
 });
 
 Myo.on("fist", () => {
@@ -42,7 +52,8 @@ Myo.on("fist", () => {
 	for(var i = 1; i < 10; i++)
 		if(digits[i] > digits[imax])
 			imax = i;
-	num = (Math.floor(num / 10) * 10 + imax) * 10;
+	// num = num  * 10 + imax;
+	document.getElementById("answer-text").innerHTML += imax.toString();
 	shiftReg = []
 })
 
